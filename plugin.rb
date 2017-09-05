0fba43db7118d7b5774da3e8bbadf9766dfa3061
David Taylor <david@taylorhq.com>
2017-09-05 02:28:41 +0100

    Only initialise if Rails is in development mode

--- plugin.rb
+++ plugin.rb
@@ -10,40 +10,42 @@ PLUGIN_NAME ||= 'discourse-dev-login'.freeze
 
 register_asset "stylesheets/dev-login.scss"
 
-after_initialize do
-  module ::DiscourseDevLogin
-    class Engine < ::Rails::Engine
-      engine_name PLUGIN_NAME
-      isolate_namespace DiscourseDevLogin
-    end
-  end
-
-  class DiscourseDevLogin::DevLoginController < ::ApplicationController
-    skip_before_filter :preload_json, :check_xhr, :redirect_to_login_if_required
-    def on_request
-      params.require(:user)
-      user = User.find_by_username_or_email(params[:user])
-      raise Discourse::NotFound if user.blank?
-
-      log_on_user(user)
-
-      redirect_to '/'
+if Rails.env.development?
+  after_initialize do
+    module ::DiscourseDevLogin
+      class Engine < ::Rails::Engine
+        engine_name PLUGIN_NAME
+        isolate_namespace DiscourseDevLogin
+      end
     end
 
-    def list_users
-      usernames = User.all.limit(50).pluck(:username)
+    class DiscourseDevLogin::DevLoginController < ::ApplicationController
+      skip_before_filter :preload_json, :check_xhr, :redirect_to_login_if_required
+      def on_request
+        params.require(:user)
+        user = User.find_by_username_or_email(params[:user])
+        raise Discourse::NotFound if user.blank?
 
-      render json: { users: usernames }
+        log_on_user(user)
+
+        redirect_to '/'
+      end
+
+      def list_users
+        usernames = User.all.limit(50).pluck(:username)
+
+        render json: { users: usernames }
+      end
     end
-  end
 
-  DiscourseDevLogin::Engine.routes.draw do
-    get '/' => 'dev_login#on_request'
-    get '/users' => 'dev_login#list_users'
-  end
+    DiscourseDevLogin::Engine.routes.draw do
+      get '/' => 'dev_login#on_request'
+      get '/users' => 'dev_login#list_users'
+    end
 
-  ::Discourse::Application.routes.append do
-    mount ::DiscourseDevLogin::Engine, at: '/dev-login'
-  end
+    ::Discourse::Application.routes.append do
+      mount ::DiscourseDevLogin::Engine, at: '/dev-login'
+    end
 
+  end
 end
